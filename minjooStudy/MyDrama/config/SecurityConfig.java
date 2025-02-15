2f0dfb38388a479208994ba1dfe9179feeab1efa
KimMinJoo <fudnr4010@naver.com>
2025-02-15 17:20:14 +0900

    회원가입 비밀번호 일치,불일치 확인 로그인 공부

--- minjooStudy/MyDrama/config/SecurityConfig.java
+++ minjooStudy/MyDrama/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
 
     @Autowired
@@ -24,6 +23,26 @@ public class SecurityConfig {
 
     @Bean // 스프링 컨테이너에 올라가는 객체 (Bean)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**","/img/**","/favicon.ico","/error").permitAll()
+                        .requestMatchers("/","/members/**","/item/**","/images/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // /admin/** admin 밑에 있는 하위 어떤 url 포함 됩니다. 롤 ADMIN
+                        .anyRequest().authenticated() //위에를 제외한 모든 url 맴핑은 모두 로그인이 되어야 접속 가능
+                )
+                //로그인을 할 경우 로그인 페이지가 나오는 경우는 관리자가 하는거지만, submit으로 로그인을 하게 된다면, formLogin으로 들어가게 된다.
+                .formLogin(formLogin -> formLogin  // form 로그인 경우 여기로 온다.
+                        .loginPage("/member/signin")  // 로그인 페이지는 /member/signin이다. (url 매핑이라고 생각하면된다.)
+                        .defaultSuccessUrl("/")  // 로그인 성공시
+                        .usernameParameter("userId")  // 로그인에 필요한 파라미터("userId")이다.
+                        .failureUrl("/member/signin/error")  // 실패시 이동하는 매핑이다.
+
+                ).logout(logout ->logout  // 로그아웃일 경우
+                        .logoutRequestMatcher(new AntPathRequestMatcher("member/logout"))
+                        .logoutSuccessUrl("/"));
+
+        //exceptionHandling 예외처리 핸들링 예외처리가 발생하면 CustomAuthenticationEntryPoint 클래스 위임
+        http.exceptionHandling(exception -> exception
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
     }
@@ -32,4 +51,9 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Autowired
+    // AuthenticationManagerBuilder -> UserDetailService를 구현하고 그 객체 MemberService 지정과 동시에 비밀번호를 암호화 암호화 한 후 MemberService에 들어가서 LoadUserByUsername을 실행시켜준다.
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
 }
--- minjooStudy/MyDrama/service/MemberService.java
+++ minjooStudy/MyDrama/service/MemberService.java
@@ -3,13 +3,19 @@ package com.MyDrama.service;
 import com.MyDrama.entity.Member;
 import com.MyDrama.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor  // 자동주입해준다.
-public class MemberService {
+// MemberService는 로그인을 확인하는 용도이기 때문에 MemberService에 부모를 심어줘야해서 UserDetailService로 상속을 받는다.
+// UseDetailsService 회원 정보를 가져오는 역활
+public class MemberService implements UserDetailsService {  // UserDetailService는 인터페이스이기 때문에 오버라이딩 해야한다.
 
     private final MemberRepository memberRepository;
 
@@ -32,5 +38,21 @@ public class MemberService {
         if(findMember !=null){
             throw new IllegalStateException("이미 가입된 번호입니다.");
         }
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {  // userId가 넘어온다.
+        // 로그인 시 userId를 받아서 DB에서 회원 정볼르 찾는다.
+        Member member = memberRepository.findByUserId(userId);
+        if(member==null) {  // userId가 null 값이라면?
+            throw new UsernameNotFoundException(userId);  // 예외처리
+        }
+        //빌더 패턴 (객체를 리턴한다.)
+        //Spring Security의 UserDetails 객체 생성
+        return User.builder().username(member.getUserId())  // User로 변경 후 검사를 한다.
+                .password(member.getPassword())  // 비밀번호 설정 (DB에 암호화된 비밀번호 저장됨)
+                .roles(member.getRole().toString())  // 권한도 검사하고
+                .build(); // 된다면 로그인이 된다.
     }
 }
