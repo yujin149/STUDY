4a47cb28076065123701c7cd388d97cd35131e5a
KimMinJoo <fudnr4010@naver.com>
2025-02-17 21:47:56 +0900

    회원 정보 수정 삭제 공부

--- minjooStudy/MyDrama/service/MemberService.java
+++ minjooStudy/MyDrama/service/MemberService.java
@@ -1,12 +1,17 @@
 package com.MyDrama.service;
 
+import com.MyDrama.dto.MemberFormDto;
+import com.MyDrama.dto.MemberupdateDto;
+import com.MyDrama.dto.PasswordChangeFormDto;
 import com.MyDrama.entity.Member;
 import com.MyDrama.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService implements UserDetailsService {  // UserDetailService는 인터페이스이기 때문에 오버라이딩 해야한다.
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member saveMember(Member member){  // controller에서 saveMember를 불렀을 때
         validateDuplicateMember(member);
@@ -54,5 +60,39 @@ public class MemberService implements UserDetailsService {  // UserDetailService
                 .password(member.getPassword())  // 비밀번호 설정 (DB에 암호화된 비밀번호 저장됨)
                 .roles(member.getRole().toString())  // 권한도 검사하고
                 .build(); // 된다면 로그인이 된다.
+    }
+
+    //수정
+
+    public void updateMember(MemberupdateDto memberupdateDto){
+
+        Member member = memberRepository.findById(memberupdateDto.getId()).orElseThrow(EntityNotFoundException::new); // 멤버 리포지토리에서 id를 찾는데 없으면 예외처리해
+
+        member.updateMember(memberupdateDto);
+    }
+
+    //비밀번호 수정
+    public void updatePassword(PasswordChangeFormDto passwordChangeFormDto){
+        Member member = memberRepository.findById(passwordChangeFormDto.getId()).orElseThrow(()->new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        if(!passwordEncoder.matches(passwordChangeFormDto.getCurrentPassword(), member.getPassword())){
+            throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+        }
+        if (!passwordChangeFormDto.getNewPassword().equals(passwordChangeFormDto.getConfirmPassword())) {
+            throw new IllegalArgumentException("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        }
+
+        member.setPassword(passwordEncoder.encode(passwordChangeFormDto.getNewPassword()));
+        memberRepository.save(member);
+    }
+
+    //회원 정보 삭제
+
+    public void deleteMember(String userId) {
+        Member member = memberRepository.findByUserId(userId);
+        if (member == null) {
+            throw new IllegalArgumentException("해당 회원을 찾을 수 없습니다.");
+        }
+        memberRepository.delete(member);
     }
 }
